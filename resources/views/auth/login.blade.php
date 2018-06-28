680ee687e47130a49281bc280797f4e5c89f7729
tjomega03 <37372913+tjomega03@users.noreply.github.com>
2018-06-28 19:19:20 +0000

    Auth Views Styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
-
+@section('stylesheet')
+    <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="the-card">
+                <div class="card-header the-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
@@ -51,11 +53,11 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-sm view-btn">
                                     {{ __('Login') }}
                                 </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                <a class="btn btn-sm info-text" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
                                 </a>
                             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
-
+@section('stylesheet')
+    <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+            <div class="the-card">
+                <div class="card-header the-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
@@ -107,7 +109,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn view-btn">
                                     {{ __('Register') }}
                                 </button>
                             </div>
