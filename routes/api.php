8e2ed454e575d0917ea613ba3530aba9a1af1f39
tjomega03 <37372913+tjomega03@users.noreply.github.com>
2018-06-29 20:24:03 +0000

    API Endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+Use App\Booking;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +16,35 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('bookings', 'BookingsController@index');
+Route::get('bookings/{id}', 'BookingsController@show');
+Route::post('bookings', 'BookingsController@store');
+Route::put('bookings/{id}', 'BookingsController@update');
+Route::delete('bookings/{id}', 'BookingsController@delete');
+
+
+Route::get('rooms', 'MeetingRoomsController@index');
+Route::get('rooms/{id}', 'MeetingRoomsController@show');
+Route::post('rooms', 'MeetingRoomsController@store');
+Route::put('rooms/{id}', 'MeetingRoomsController@update');
+Route::delete('rooms/{id}', 'MeetingRoomsController@delete');
+
+Route::get('team-members', 'TeamMembersController@index');
+Route::get('team-members/{id}', 'TeamMembersController@show');
+Route::post('team-members', 'TeamMembersController@store');
+Route::put('team-members/{id}', 'TeamMembersController@update');
+Route::delete('team-members/{id}', 'TeamMembersController@delete');
+
+
+Route::get('teams', 'TeamController@index');
+Route::get('teams/{id}', 'TeamsController@show');
+Route::post('teams', 'TeamsController@store');
+Route::put('teams/{id}', 'TeamsController@update');
+Route::delete('teams/{id}', 'TeamsController@delete');
+
+Route::get('attendance-lists', 'AttendanceListsController@index');
+Route::get('attendance-lists/{id}', 'AttendanceListsController@show');
+Route::post('attendance-lists', 'AttendanceListsController@store');
+Route::put('attendance-lists/{id}', 'AttendanceListsController@update');
+Route::delete('attendance-lists/{id}', 'AttendanceListsController@delete');
