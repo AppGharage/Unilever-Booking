551cb948615ae3f7dc7c8ee397a361bb8c9a667d
Baffour Adu Boampong <baffouraduboampong@gmail.com>
2018-08-18 20:16:28 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return redirect()->to('/dashboard');
+});
+
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('/bookings', 'BookingsController@index');
+Route::get('/rooms', 'MeetingRoomsController@index');
+Route::get('/rooms/create', 'MeetingRoomsController@create');
+Route::get('/create', 'BookingsController@create');
 
 Route::resource('bookings','BookingsController');
 Route::resource('team-members','TeamMembersController');
@@ -17,13 +28,5 @@ Route::resource('teams','TeamsController');
 Route::resource('rooms','MeetingRoomsController');
 Route::resource('attendance-lists','AttendanceListsController');
 
-Route::get('/bookings', 'BookingsController@index');
-Route::get('/rooms', 'MeetingRoomsController@index');
-Route::get('/rooms/create', 'MeetingRoomsController@create');
-Route::get('/create', 'BookingsController@create');
-Auth::routes();
 
-// GETTING VALUES
 
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
-Route::get('/', 'DashboardController@index');
